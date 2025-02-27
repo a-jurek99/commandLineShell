88f8fa1c89b9d1cc3aeeadd984833968c7fb3be4
JDB <legodude17@users.noreply.github.com>
2025-02-27 13:44:26 -0800

    Add ability to run a file by provided the shell with arguments

--- CommandShell.java
+++ CommandShell.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
@@ -7,34 +9,78 @@ public class CommandShell {
     /**
      * Entry point of the program, simply loops until told to exit.
      * Each loop involves getting the inputted command, adding it to history,
-     * parsing it, and finally executing it
+     * parsing it, and finally executing it.
+     * When provided with command line arguments, interprets them as files, and
+     * attempts to execute each line of each file as if it was a command.
      * 
-     * @param args The command line arguments passed to the shell, currently ignores
+     * @param args The command line arguments passed to the shell, if any
      */
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Executor executor = new Executor();
-        while (true) {
-            System.out.print("> ");
-            String userInput = scan.nextLine();
-            executor.addHistory(userInput);
-            Parser parser = new Parser(userInput);
-            ProcessNode root;
-            try {
-                root = parser.parse();
-            } catch (Parser.SyntaxException ex) {
-                System.out.println(ex.toString());
-                continue;
+        if (args.length == 0) {
+            Scanner scan = new Scanner(System.in);
+            Executor executor = new Executor();
+            while (true) {
+                System.out.print("> ");
+                String userInput = scan.nextLine();
+                executor.addHistory(userInput);
+                Parser parser = new Parser(userInput);
+                ProcessNode root;
+                try {
+                    root = parser.parse();
+                } catch (Parser.SyntaxException ex) {
+                    System.out.println(ex.toString());
+                    continue;
+                }
+                if (root == null) {
+                    continue;
+                }
+                // System.out.println(root.toString());
+                boolean shouldExit = executor.execute(root);
+                if (shouldExit) {
+                    break;
+                }
             }
-            if (root == null) {
-                continue;
-            }
-            // System.out.println(root.toString());
-            boolean shouldExit = executor.execute(root);
-            if (shouldExit) {
-                break;
+            scan.close();
+        } else {
+            Executor executor = new Executor();
+            for (int i = 0; i < args.length; i++) {
+                File file = new File(args[i]);
+                if (!file.exists() || !file.canRead()) {
+                    System.out.println("ERROR: File not readable: " + args[i]);
+                    continue;
+                }
+                Scanner scan;
+                try {
+                    scan = new Scanner(file);
+                } catch (FileNotFoundException ex) {
+                    System.out.println("ERROR: File not found: " + args[i]);
+                    continue;
+                }
+                int line = 1;
+                while (scan.hasNextLine()) {
+                    String input = scan.nextLine();
+                    executor.addHistory(input);
+                    Parser parser = new Parser(input);
+                    ProcessNode root;
+                    try {
+                        root = parser.parse();
+                    } catch (Parser.SyntaxException ex) {
+                        System.out.println("Syntax error in " + args[i] + " on line " + line + ":");
+                        System.out.println(ex.toString());
+                        continue;
+                    }
+                    if (root == null) {
+                        continue;
+                    }
+                    // System.out.println(root.toString());
+                    boolean shouldExit = executor.execute(root);
+                    if (shouldExit) {
+                        break;
+                    }
+                    line += 1;
+                }
+                scan.close();
             }
         }
-        scan.close();
     }
 }
