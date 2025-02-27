dffee65f97e6a3e78a56d15c48fcb1f6d162fc7b
JDB <legodude17@users.noreply.github.com>
2025-02-27 14:25:28 -0800

    Fix build script so it includes external library

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
+# Delete previous build
 rm -rf build
+# Unpack the Apache library and put it in the right place
+cd lib
+jar xf commons-text-1.13.0.jar
+mv org ..
+# Create build directory
+cd ..
 mkdir build
-javac -d build -Xlint -cp "lib/*:." CommandShell.java
+# Compile the program
+javac -d build -Xlint CommandShell.java
 cd build
-jar cvfe CommandShell.jar CommandShell *.class ../lib/commons-text-1.13.0.jar
+# Pack the program into a jar, including the necessary Apache classes
+jar cvfe CommandShell.jar CommandShell *.class\
+ ../org/apache/commons/text/similarity/LevenshteinDistance.class\
+ ../org/apache/commons/text/similarity/EditDistance.class\
+ ../org/apache/commons/text/similarity/SimilarityScore.class\
+ ../org/apache/commons/text/similarity/ObjectSimilarityScore.class\
+ ../org/apache/commons/text/similarity/SimilarityInput.class\
+ ../org/apache/commons/text/similarity/SimilarityCharacterInput.class
+cd ..
+# Delete intermediate files
+rm -rf org lib/org lib/META-INF build/*.class
